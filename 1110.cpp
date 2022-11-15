382410f34c08eaa060def937096f5f7d1186ec90
jihoon50 <thswlgns50@naver.com>
2022-11-16 01:06:15 +0900

    solve 1110

--- 1110.cpp
+++ 1110.cpp
@@ -2,19 +2,21 @@
 #include<string>
 
 using namespace std;
-int cycle(string N){
-    if (N.length()) N="a"+ N;
-    cout << N<<endl;
-    return 0;
 
+int cnt = 0;
+int N;
+void cycle(int n){
+    cnt ++;
+    int n2 = n/10 + n%10;
+
+    n = (n%10)*10+ (n2%10);
+
+    if(n==N) return ;
+    else cycle(n);
 }
 int main(void){
-    string N;
     cin >> N;
     cycle(N);
-   if(N.compare("ab")) cout << "yes";
-   else cout << "no";
-
-
+    cout << cnt;
 
 }
